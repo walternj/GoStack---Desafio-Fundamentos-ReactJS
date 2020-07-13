a39cd1708475aa57bdfb62438c47b7fe421fb5ed
Walter Nunes Jr <walternj@hotmail.com>
2020-07-13 18:18:23 +0200

    last time a make this sjkqshjhsqd_

--- src/App.js
+++ src/App.js
@@ -1,64 +1,4 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react";
-import api from './services/api';
 
-import "./styles.css";
-
-function App() {
-  const [repositories, setRepositories] = useState([])
-
-  useEffect(() => {
-    function loadRepositories() {
-      api.get('repositories').then(response => 
-        setRepositories(response.data)
-      )
-    }
-
-    loadRepositories();
-  },[])
-
-  
-
-  async function handleAddRepository() {
-    const response = await api.post('repositories', {
-      title: `Repository - ${Date.now()}`,
-      url: `https://github.com/${Date.now()}`,
-      techs: ["Node.js", "ReactJS"]
-    });
-    
-    const repository = response.data;
-    setRepositories([...repositories, repository])
-  }
-
-  async function handleRemoveRepository(id, e) {
-    e.preventDefault()
-    await api.delete(`repositories/${id}`);
-
-    setRepositories(repositories.filter((repository) => repository.id !== id));
-   
-  }
-
-  return (
-    <div>
-      <ul data-testid="repository-list">
-      { 
-        repositories.map(repository => 
-          <li key={repository.id}>
-            {repository.title}
-            <button onClick={(e) => handleRemoveRepository(repository.id, e)}>
-              Remover
-            </button>
-          </li>
-        )
-      }
-      </ul>
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React, { useState, useEffect } from "react";
 import api from './services/api';
 
@@ -89,9 +29,7 @@ function App() {
     await api.delete(`repositories/${id}`)
     console.log(id)
 
-    await api.get('repositories').then(response => {
-      setRepositories(response.data);
-    });
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
@@ -114,4 +52,4 @@ function App() {
 }
 
 export default App;
->>>>>>> 695647403e0f09a6cb8b5a1b46303d16787d4d5c
+
